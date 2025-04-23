d3614ff4ed791b54c9ae253b68c2ed6774eec6a9
cbramhesh <bchaugule45@gmail.com>
2025-04-23 11:31:47 +0530

    Intiial setup

---
+++ topics/01-arrays/code/src/com/bramhesh/Arrays.java
@@ -0,0 +1,14 @@
+package com.bramhesh;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        int a = 19;
+        String name = "Bramhesh Chaugule";
+        System.out.println(name);
+
+        String[] arr = new String[4];
+        System.out.println(arr[0]);
+
+    }
+}
