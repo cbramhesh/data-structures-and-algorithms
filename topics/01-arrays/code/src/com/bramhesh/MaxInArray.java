e5ff75670f3a1191833353d45652b823d3e7f3d9
cbramhesh <bchaugule45@gmail.com>
2025-04-26 13:31:01 +0530

    Max element in an Array

---
+++ topics/01-arrays/code/src/com/bramhesh/MaxInArray.java
@@ -0,0 +1,19 @@
+package com.bramhesh;
+
+public class MaxInArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 9, 20, 5, 10, 2, 9};
+        System.out.println(max(arr));
+    }
+
+    static int max(int[] arr) {
+        int maxValue = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxValue) {
+                maxValue = arr[i];
+            }
+        }
+        return maxValue;
+    }
+
+}
