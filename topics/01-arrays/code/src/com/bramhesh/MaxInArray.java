283fde595552fa474f3f12a1302095ac6ef9a93b
cbramhesh <bchaugule45@gmail.com>
2025-04-26 19:51:16 +0530

    Implements Max in range and reverse array

--- topics/01-arrays/code/src/com/bramhesh/MaxInArray.java
+++ topics/01-arrays/code/src/com/bramhesh/MaxInArray.java
@@ -3,7 +3,18 @@ package com.bramhesh;
 public class MaxInArray {
     public static void main(String[] args) {
         int[] arr = {1, 3, 9, 20, 5, 10, 2, 9};
+        System.out.println(maxInRange(arr, 4, 7));
         System.out.println(max(arr));
+    }
+
+    static int maxInRange(int[] arr, int start, int end) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[max] > max) {
+                max = arr[max];
+            }
+        }
+        return max;
     }
 
     static int max(int[] arr) {
--- topics/01-arrays/code/src/com/bramhesh/Swap.java
+++ topics/01-arrays/code/src/com/bramhesh/Swap.java
@@ -6,8 +6,36 @@ public class Swap {
     public static void main(String[] args) {
         int[] arr = {1, 3, 9, 20, 5, 10, 2, 9};
         swap(arr, 3, 5);
+        reverseArray(arr);
+        reverse(arr);
 
         System.out.println(Arrays.toString(arr));
+        for (int ele: arr) {
+            System.out.print(ele + " ");
+        }
+    }
+        static void reverseArray(int[] arr)  {
+            int start = 0;
+            int end = arr.length-1;
+            while (start < end) {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+    static void reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+
     }
 
     static void swap(int[] arr, int index1, int index2) {
