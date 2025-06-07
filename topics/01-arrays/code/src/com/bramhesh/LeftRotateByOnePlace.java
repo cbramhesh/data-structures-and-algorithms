ed0895b5cf49775a92d43e45501ea86211bf9ab8
cbramhesh-personal <bchaugule45@gmail.com>
2025-06-07 11:37:40 +0530

    [Make it non static method]

--- topics/01-arrays/code/src/com/bramhesh/LeftRotateByOnePlace.java
+++ topics/01-arrays/code/src/com/bramhesh/LeftRotateByOnePlace.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class LeftRotateByOnePlace {
 
-    public static int[] leftRotateByOnePlace(int[] nums) {
+    public int[] leftRotateByOnePlace(int[] nums) {
         int temp = nums[0];
         for (int i = 1; i < nums.length; i++) {
             nums[i - 1] = nums[i];
@@ -15,6 +15,8 @@ public class LeftRotateByOnePlace {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6};
-        System.out.println(Arrays.toString(leftRotateByOnePlace(arr)));
+        LeftRotateByOnePlace obj = new LeftRotateByOnePlace();
+        int[] result = obj.leftRotateByOnePlace(arr);
+        System.out.println(Arrays.toString(result));
     }
 }
